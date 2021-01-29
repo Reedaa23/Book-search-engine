ae9289559cfa1ce85f1d90e23be5b9fdd46add54
Reda <reedaa23.git@gmail.com>
2021-01-29 23:22:47 +0100

    Fix ebooks database initialization

--- my_gutenberg/management/commands/init_ebooks.py
+++ my_gutenberg/management/commands/init_ebooks.py
@@ -3,13 +3,14 @@ from my_gutenberg.models import Ebook
 from my_gutenberg.serializers import EbookSerializer
 import time
 from my_gutenberg.management.commands.importer import get_ebook
-
+from django.contrib.admin.models import LogEntry
 from collections import Counter
 import re
 import urllib
 import numpy as np
 import networkx as nx
 import itertools
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Command(BaseCommand):
     help = 'Initialize ebooks database'
@@ -21,6 +22,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.stdout.write('['+time.ctime()+'] Initializing ebooks database')
 
+        LogEntry.objects.all().delete()
         Ebook.objects.all().delete()
 
         first_ebook_id = kwargs['first_ebook']
@@ -44,12 +46,17 @@ class Command(BaseCommand):
         books_urls = list(itertools.chain(*a))
 
 
-        self.stdout.write('['+time.ctime()+'] Calculating Jaccard Matrix...')
+        self.stdout.write('['+time.ctime()+'] Calculating Jaccard Matrix and Graph...')
         txt_list = []
+        str_list = []
         special_letters = 'àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'
         decoding = "ISO-8859-1"
         n = len(books_urls)
         MATRIX = np.zeros((n, n))
+        # Graph 
+        threshold = 0.7
+        G = nx.Graph()
+        
 
         # Remplissage ligne par ligne
         for i in range(len(books_urls)):
@@ -58,27 +65,32 @@ class Command(BaseCommand):
             if i ==0:
                 # Ouvir l'url, lire puis décoder le contenu
                 txt = urllib.request.urlopen(books_urls[i]).read().decode(decoding)
-                # Ajouter le texte décodé dans la liste des textes 
                 txt_list.append(txt)
+                # Ajouter le texte décodé dans la liste des textes 
+                print(books_urls[i])
+                # Rendre le texte en minuscule, diviser en mots, mettre ces mots dans une liste
+                s1 = re.split('[^a-zA-Z0-9'+special_letters+']', txt.lower())
+                str1 = list(filter(lambda x: x !="", s1))
+                str_list.append(str1)
             else:
-                txt = txt_list[i]
+                str1 = str_list[i]
 
-            # Rendre le texte en minuscule, diviser en mots, mettre ces mots dans une liste
-            s1 = re.split('[^a-zA-Z0-9'+special_letters+']', txt.lower())
-            str1 = list(filter(lambda x: x !="", s1))
-            
+
             for j in range(len(books_urls))[i + 1:]:
+
+                print("    ",books_urls[j])
                 num = 0
                 denom = 0
                 # Si premier livre, décoder et ajouter le texte décodé des autres livres, sinon  
                 if i==0:
                     txt2 = urllib.request.urlopen(books_urls[j]).read().decode(decoding)
+                    s2 = re.split('[^a-zA-Z0-9'+special_letters+']', txt2.lower())
+                    str2 = list(filter(lambda x: x !="", s2))
+                    str_list.append(str2)
                     txt_list.append(txt2)
                 else:
-                    txt2 = txt_list[j]
+                    str2 = str_list[j]
 
-                s2 = re.split('[^a-zA-Z0-9'+special_letters+']', txt2.lower())
-                str2 = list(filter(lambda x: x !="", s2))
 
                 # Fusionner les deux listes de mots 
                 D = str1 + str2
@@ -87,6 +99,7 @@ class Command(BaseCommand):
                 d2 = Counter(str2)
 
                 # Pour chaque mot dans les deux livres, récuperer son nombre d'occurence
+                # print( '['+time.ctime()+'] commence comptage')
                 for m in D:
                     k1 = d1[m]
                     k2 = d2[m]
@@ -95,21 +108,24 @@ class Command(BaseCommand):
                     num = num + MAX - MIN
                     denom = denom + MAX
                 MATRIX[i][j] = num / denom
+                # print('['+time.ctime()+']fini comptage')
 
-        self.stdout.write('['+time.ctime()+']  Jaccard Matrix calculated...')
-         
-        # Graph 
-        self.stdout.write('['+time.ctime()+'] Calculating Graph...')
-        threshold = 0.7
-        G = nx.Graph()
-
-        for i in range(len(books_urls)):
-            for j in range(len(books_urls))[i+1:]:
                 distance = MATRIX[i][j]
                 if distance < threshold:
                     G.add_edge(books_urls[i], books_urls[j], weight = distance)
+
+            vectorizer = TfidfVectorizer(max_features = 10, lowercase=False, stop_words = 'english')
+            X = vectorizer.fit_transform(str_list[i])
+            keywords = list(vectorizer.vocabulary_.keys())
+            kw = ','.join(keywords)
+            e = Ebook.objects.get(content_url = books_urls[i])
+            e.keywords = kw
+            e.save()
+            
+
+        self.stdout.write('['+time.ctime()+']  Jaccard Matrix calculated...')
         self.stdout.write('['+time.ctime()+'] Graph generated...')
-        
+      
         # Closeness centrality, return {"vetex" : cc, "vertex" : cc }
         self.stdout.write('['+time.ctime()+'] Calculating CC...')
         CC = nx.closeness_centrality(G)
@@ -128,4 +144,18 @@ class Command(BaseCommand):
 
         self.stdout.write('['+time.ctime()+']  Ranking calculated...')
 
+        self.stdout.write('['+time.ctime()+']  Calculating neighbors...')
 
+        for node in G.nodes:
+            voisins = G.neighbors(node)
+            voisins_ids = []
+            for voisin in voisins:
+                b = Ebook.objects.filter(content_url=voisin).values_list("id")
+                voisin_id = list(itertools.chain(*b))[0]
+                voisins_ids.append(str(voisin_id))
+                neighbors = '/'.join(voisins_ids)
+                e = Ebook.objects.get(content_url = voisin)
+                e.neighbors = neighbors
+                e.save()
+
+        self.stdout.write('['+time.ctime()+'] END...')
