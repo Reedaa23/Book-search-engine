f8b92af50b85ab0ca83265112031a6b91b3c416b
Amine <benrabah.src@gmail.com>
2021-01-31 15:37:15 +0100

    fix fill_matrix script and add keywords to init_ebooks

--- my_gutenberg/management/commands/fill_matrix.py
+++ my_gutenberg/management/commands/fill_matrix.py
@@ -29,89 +29,91 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.stdout.write('['+time.ctime()+'] Filling Jaccard matrix')
 
-        # LogEntry.objects.all().delete()
-        # Ebook.objects.all().delete()
-
         first_ebook_id = kwargs['first_ebook']
         last_ebook_id = kwargs['last_ebook']
         
         self.stdout.write('['+time.ctime()+'] Database initializing terminated.')
         
+
         books_urls = []
-        for ebook_number in range(first_ebook_id, last_ebook_id +1):
-            try:
-                url = get_ebook(ebook_number)['content_url']
-                books_urls.append(url)
-            except FileNotFoundError:
-                continue
-            
+
+
+        a = Ebook.objects.filter(id__range=[first_ebook_id, last_ebook_id]).values_list("content_url")
+        books_urls = list(itertools.chain(*a))
+        languages = Ebook.objects.values_list("languages")
+        languages = list(itertools.chain(*languages))
+
 
         decoding = "ISO-8859-1"
         special_letters = 'àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'
         n = len(books_urls)
         jaccard_file_name = './matrix.json'
         threshold = 0.7
-        
+
         if os.path.isfile(jaccard_file_name):
             print ("File exist")
             f = open(jaccard_file_name, 'r')
             MATRIX = json.load(f)
             f.close()
+        else:
+            print ("File not exist")
+            print("Please run init_ebooks first")
+            MATRIX = np.zeros((n, n))
 
-            file_str = open('str.json', 'r')
-            str_list = json.load(file_str)
-            file_str.close()
 
-            file_line = open("lastLine.json", 'r')
-            next_line = json.load(file_line) + 1
+        
+        lastLine_file_name = "./lastLine.json"
+        if os.path.isfile(lastLine_file_name):
+            file_line = open(lastLine_file_name, 'r')
+            next_line = json.load(file_line)
             file_line.close()
+        else:
+            next_line = 0
 
+
+
+
+        graph_file_name = "./graph.json"
+        if os.path.isfile(graph_file_name):
             file_graph = open("graph.json", "r")
             g_data = json.load(file_graph)
             file_graph.close()
             G = json_graph.node_link_graph(g_data)
-            print(G.nodes())
-
-
-
         else:
-            print ("File not exist")
-            MATRIX = np.zeros((n, n))
-            next_line = 0
-            str_list = []
             G = nx.Graph()
-        
+
+
+
+
+        words_file_name = './str.json'
+        if os.path.isfile(words_file_name):
+            file_str = open(words_file_name, 'r')
+            str_list = json.load(file_str)
+            file_str.close()
+        else:
+            str_list = []
+
+
+
+
+
         print('initial matrix')
         print(DataFrame(MATRIX))
         print('['+time.ctime()+'] Calculating Jaccard Matrix...')
 
 
         for i in range(next_line,len(books_urls)):
-            print(i)
-
-            if i ==0:
-                txt = urllib.request.urlopen(books_urls[i]).read().decode(decoding)
-                print(books_urls[i])
-                s1 = re.split('[^a-zA-Z0-9'+special_letters+']', txt.lower())
-                str1 = list(filter(lambda x: x !="", s1))
-                str_list.append(str1)
-            else:
+            try:
                 str1 = str_list[i]
+                print("Row number : {} ".format(i))
+            except IndexError:
+                exit()
+                
 
             for j in range(len(books_urls))[i + 1:]:
-                print("    ",books_urls[j])
                 num = 0
                 denom = 0
-
-                if i==0:
-                    txt2 = urllib.request.urlopen(books_urls[j]).read().decode(decoding)
-                    s2 = re.split('[^a-zA-Z0-9'+special_letters+']', txt2.lower())
-                    str2 = list(filter(lambda x: x !="", s2))
-                    str_list.append(str2)
-                    with open('str.json','w', encoding='utf-8') as str_file:
-                        json.dump(str_list, str_file, ensure_ascii=False, indent=4)
-                else:
-                    str2 = str_list[j]
+                str2 = str_list[j]
 
                 D = str1 + str2
                 d1 = Counter(str1)
@@ -135,16 +137,27 @@ class Command(BaseCommand):
 
             CC = nx.closeness_centrality(G)
             ranking = sorted(CC.items(), key=lambda item: item[1], reverse=True)
-            print(ranking)
             for tup in ranking:
                 url = tup[0]
-                print("url : ", url)
                 rank = ranking.index(tup) + 1
                 e = Ebook.objects.get(content_url = url)
                 e.rank = rank
                 e.save()
+        
 
-      
+            # if languages[i] == "en":
+            #     vectorizer = TfidfVectorizer(max_features = 10, lowercase=False, stop_words = 'english')
+            # else:
+            #     vectorizer = TfidfVectorizer(max_features = 10, lowercase=False, stop_words = safe_get_stop_words(languages[i]))
+
+            # X = vectorizer.fit_transform(str_list[i])
+            # keywords = list(vectorizer.vocabulary_.keys())
+            # kw = ','.join(keywords)
+            # e = Ebook.objects.get(content_url = books_urls[i])
+            # e.keywords = kw
+            # e.save()
+
+
             # Finished a line ? write in the file
             try:
                 matrix = MATRIX.tolist()
@@ -152,9 +165,29 @@ class Command(BaseCommand):
                 matrix = MATRIX 
             with open('matrix.json','w', encoding='utf-8') as f:
                 json.dump(matrix, f, ensure_ascii=False, indent=4)
-            with open('lastLine.json', 'w', encoding='utf-8') as ff:
+            with open(lastLine_file_name, 'w', encoding='utf-8') as ff:
                 json.dump(i, ff, ensure_ascii=False, indent=4)
+        
 
         print('['+time.ctime()+'] Calculating Jaccard Matrix terminated...')
         print(DataFrame(MATRIX))
-        print(G.edges())
+        os.remove(words_file_name)
+        os.remove(lastLine_file_name)
+        os.remove(graph_file_name)
+
+
+        for node in G.nodes:
+            voisins = G.neighbors(node)
+            neighbors = ""
+            for voisin in voisins:
+                m = re.search('http://www.gutenberg.org/files/([0-9]+)/', voisin)
+                if m:
+                    id = m.group(1)
+                    neighbors += "/"+id
+                else:
+                    continue
+
+            e = Ebook.objects.get(content_url=node)
+            e.neighbors = neighbors
+            e.save()
+
